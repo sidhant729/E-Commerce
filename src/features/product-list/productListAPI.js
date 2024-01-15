45a2610209b0a9d9948d46ea49bd382a7872a07a
sidhant729 <sidhant1841.be21@chitkara.edu.in>
2024-01-15 15:41:45 +0530

    Adding Upto Category and Brands API

--- src/features/product-list/productListAPI.js
+++ src/features/product-list/productListAPI.js
@@ -1,5 +1,6 @@
-export function fetchAllProducts(amount = 1) {
+export function fetchAllProducts() {
   return new Promise(async (resolve) =>{
+    //TODO: we will not hard-code server URL here
     const response = await fetch('http://localhost:8080/products') 
     const data = await response.json()
     resolve({data})
@@ -7,21 +8,52 @@ export function fetchAllProducts(amount = 1) {
   );
 }
 
-export function fetchProductsByFilters(filter) {
-  // filter = {"category":"smartphone"}
-  // TODO : on server we will support multi values
+export function fetchProductsByFilters(filter,sort,pagination) {
+  // filter = {"category":["smartphone","laptops"]}
+  // sort = {_sort:"price",_order="desc"}
+  // pagination = {_page:1,_limit=10} 
+  // TODO : on server we will support multi values in filter
   let queryString = '';
   for(let key in filter){
     const categoryValues = filter[key];
-    if(categoryValues.length > 0) {
-      const lastCategoryValue = categoryValues[categoryValues.length - 1];
-      queryString += `${key}=${filter[key]}&`
-    } 
+    if(categoryValues.length){
+      const lastCategoryValue = categoryValues[categoryValues.length-1]
+      queryString += `${key}=${lastCategoryValue}&`
+    }
   }
+  for(let key in sort){
+    queryString += `${key}=${sort[key]}&`
+  }
+  console.log(pagination)
+  for(let key in pagination){
+    queryString += `${key}=${pagination[key]}&`
+  }
+
 
   return new Promise(async (resolve) =>{
     //TODO: we will not hard-code server URL here
     const response = await fetch('http://localhost:8080/products?'+queryString) 
+    const data = await response.json()
+    const totalItems = await response.headers.get('X-Total-Count')
+    resolve({data:{products:data,totalItems:+totalItems}})
+  }
+  );
+}
+
+export function fetchCategories() {
+  return new Promise(async (resolve) =>{
+    //TODO: we will not hard-code server URL here
+    const response = await fetch('http://localhost:8080/category') 
+    const data = await response.json()
+    resolve({data})
+  }
+  );
+}
+
+export function fetchBrands() {
+  return new Promise(async (resolve) =>{
+    //TODO: we will not hard-code server URL here
+    const response = await fetch('http://localhost:8080/brands') 
     const data = await response.json()
     resolve({data})
   }
